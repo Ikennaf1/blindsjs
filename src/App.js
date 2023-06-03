536d00308dd3def64e7ca69f6d5bf4016165a48a
Ikennaf1 <iiikbros202@gmail.com>
2023-06-03 23:20:07 +0100

    Routing and scrolling fixes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import Nav from './Nav';
 import Footer from './Footer';
@@ -8,6 +8,7 @@ import V0QuickStart from './docs/v0/QuickStart';
 import V0Installation from './docs/v0/Installation';
 import V0CustomStyling from './docs/v0/CustomStyling';
 import V0CustomIcons from './docs/v0/CustomIcons';
+import ScrollToTop from './ScrollToTop';
 
 function App() {
   return (
@@ -17,14 +18,18 @@ function App() {
 
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/docs/v0/quick-start" element={<V0QuickStart />} />
+            <Route path="/docs" element={<Navigate to="/docs/v0/installation" replace={true} />} />
+            <Route path="/docs/v0" element={<Navigate to="/docs/v0/installation" replace={true} />} />
+
             <Route path="/docs/v0/installation" element={<V0Installation />} />
+            <Route path="/docs/v0/quick-start" element={<V0QuickStart />} />
             <Route path="/docs/v0/custom-styling" element={<V0CustomStyling />} />
             <Route path="/docs/v0/custom-icons" element={<V0CustomIcons />} />
           </Routes>
 
         <Footer />
       </div>
+      <ScrollToTop />
     </BrowserRouter>
   );
 }
